c43bf90134e5e85f31fb7da930ab1ee2b2ae1549
pjd9624 <54874485+pjd9624@users.noreply.github.com>
2019-09-18 12:42:39 +0900

    Add files via upload

---
+++ dan.js
@@ -0,0 +1,8 @@
+dan = prompt("원하는 단",2);
+
+for(var i = 1; i<10; i++)
+{
+	document.writeln(dan + "x" +i + "=" + (dan*i) +"<br>");
+}
+
+
---
+++ test02.js
@@ -0,0 +1,9 @@
+
+for(var i = 2; i < 10; i++){
+	document.writeln("<br><br>"+ i + "단    <br><br>");
+	for (var j =1; j < 10; j++)
+	{
+		document.writeln(i + "x" + j + "=" + i * j + "<br>");
+	}
+	document.writeln("");
+}
